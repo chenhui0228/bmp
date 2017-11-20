e1ed750931f6488620380678f6a17fc25a59d2d3
lingwb <wenboling@sf-express.com>
2017-11-20 18:51:37 +0800

    字节转MB、GB

--- server/web/src/main.js
+++ server/web/src/main.js
@@ -65,6 +65,13 @@ Vue.filter("pathFilter", function (path) {
   path = path.replace(/^\w+:\//g,'');
   return path;
 });
+Vue.filter("BytesReadable", function (bytes) {
+  if (bytes === 0) return '0 B';
+  let k = 1024, // or 1024
+    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+  return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+});
 Vue.prototype.myDateFormat = function (date, fmt) {
   var o = {
     "y+": date.getFullYear(),
