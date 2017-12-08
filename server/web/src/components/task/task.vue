4bd17bb4a786176f0d5bcb1189625b5800bd9b65
凌文波 <wenboling@sf-express.com>
2017-12-08 17:06:47 +0800

    修复了很多bug, 完善了任务的表单认证

--- server/web/src/components/task/task.vue
+++ server/web/src/components/task/task.vue
@@ -282,9 +282,9 @@
       <el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false" :beforeClose="cancelEdit">
         <el-form :model="editForm" label-width="100px" :rules="editFormRules" ref="editForm">
           <el-form-item prop="name" label="任务名">
-            <el-input v-model="editForm.name" auto-complete="off"></el-input>
+            <el-input v-model="editForm.name" auto-complete="off" disabled></el-input>
           </el-form-item>
-          <el-form-item prop="policy" label="任务策略">
+          <el-form-item prop="policy_id" label="任务策略">
             <el-select v-model="editForm.policy_id" placeholder="请选择">
               <el-option
                 v-for="policy in policies"
@@ -294,7 +294,7 @@
               </el-option>
             </el-select>
           </el-form-item>
-          <el-form-item prop="worker" label="作业机">
+          <el-form-item prop="worker_id" label="作业机">
             <el-select v-model="editForm.worker_id" placeholder="请选择">
               <el-option
                 v-for="worker in workers"
@@ -382,7 +382,7 @@
           <el-form-item prop="name" label="任务名">
             <el-input v-model="addForm.name" auto-complete="off"></el-input>
           </el-form-item>
-          <el-form-item prop="policy" label="任务策略">
+          <el-form-item prop="policy_id" label="任务策略">
             <el-select v-model="addForm.policy_id" placeholder="请选择">
               <el-option
                 v-for="policy in policies"
@@ -400,7 +400,7 @@
               </span>
             </span>
           </el-form-item>
-          <el-form-item prop="worker" label="作业机">
+          <el-form-item prop="worker_id" label="作业机">
             <el-select v-model="addForm.worker_id" placeholder="请选择">
               <el-option
                 v-for="worker in workers"
@@ -558,7 +558,7 @@
                  :close-on-click-modal="false"
                  :beforeClose="cancelCreateRecoverTask">
         <el-form :model="createRecoverTaskForm" label-width="100px" ref="createRecoverTaskForm" :rules="createRecoverTaskFormRules">
-          <el-form-item prop="worker" label="作业机">
+          <el-form-item prop="worker_id" label="作业机">
             <el-select v-model="createRecoverTaskForm.worker_id" placeholder="请选择">
               <el-option
                 v-for="worker in workers"
@@ -753,11 +753,11 @@
           name: [
             {required: true, message: '请输入任务名', trigger: 'blur'}
           ],
-          policy: [
-            {required: false, message: '请选择策略', trigger: 'blur'}
+          policy_id: [
+            {required: true, message: '请选择策略', trigger: 'blur'}
           ],
-          worker: [
-            {required: false, message: '请选择作业机', trigger: 'blur'}
+          worker_id: [
+            {required: true, message: '请选择作业机', trigger: 'blur'}
           ],
           source: [
             {required: true, message: '源地址不能为空', trigger: 'blur'}
@@ -786,8 +786,8 @@
         },
 
         createRecoverTaskFormRules: {
-          worker: [
-            {required: false, message: '请选择作业机'}
+          worker_id: [
+            {required: true, message: '请选择作业机'}
           ],
           destination: [
             {required: true, message: '目的地址不能为空', trigger: 'blur'}
@@ -1153,10 +1153,11 @@
       },
       //TODO:Recover Task
       showCreateRecoverTaskDialog: function (row) {
+        let now_date = parseInt(new Date().getTime()/1000);
         this.createRecoverTaskFormVisible = true;
         let task_suffix = util.formatDate.format(row.start_time, 'yyyyMMddhhmmss');
         let descript_suffix = util.formatDate.format(row.start_time, 'yyyy-MM-dd hh:mm:ss');
-        let name = this.currentWatchingTask.name + '_Recover_' + task_suffix;
+        let name = this.currentWatchingTask.name + '_Recover_' + task_suffix + '_' + now_date;
         let source_prefix = this.currentWatchingTask.destination; //恢复任务源地址前面一部分是该备份任务的目的地址
         let source_suffix = '';
         if(source_prefix.charAt(source_prefix.length -1) === '/'){
@@ -1185,8 +1186,14 @@
             para.destination = 'file:/' + para.destination;
             reqAddTask(user, para).then((res) => {
               this.createRecoverTaskLoading = false;
-              //NProgress.done();
-              this.openMsg('创建成功，请前往恢复任务查看','success');
+              if(res.data.exist && res.data.exist === 'True') {
+                this.$message({
+                  message: `添加失败, 任务 ${res.data.task.name} 已存在`,
+                  type: 'error'
+                });
+              }else{
+                this.openMsg('创建成功，请前往恢复任务查看','success');
+              }
               this.$refs['createRecoverTaskForm'].resetFields();
               this.createRecoverTaskFormVisible = false;
             }).catch(err=>{
@@ -1381,11 +1388,18 @@
 
             reqAddTask(user, para).then((res) => {
               this.addLoading = false;
-              //NProgress.done();
-              this.$message({
-                message: '添加成功',
-                type: 'success'
-              });
+              if(res.data.exist && res.data.exist === 'True') {
+                this.$message({
+                  message: `添加失败, 任务 ${res.data.task.name} 已存在`,
+                  type: 'error'
+                });
+              }else{
+                //NProgress.done();
+                this.$message({
+                  message: '添加成功',
+                  type: 'success'
+                });
+              }
               this.$refs['addForm'].resetFields();
               this.addFormVisible = false;
               this.getTasks();
